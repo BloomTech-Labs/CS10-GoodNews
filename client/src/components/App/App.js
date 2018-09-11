207759c8bc6f924abbf334bf85af7534286cb3a0
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-09-11 11:17:28 -0700

    create basic nav that can trigger modal

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import Nav from './Nav/Nav';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      enteredSite: false,
+      loggedIn: false,
+      signInModal: false,
+      registerModal: false
+    }
+  }
+
+  toggleModal = (modal) => {
+    const on = !this.state[modal];
+    this.setState({[modal]: on});
+  }
+
   render() {
     return (
       <div className="App">
+        <Nav toggleModal={this.toggleModal}/>
       </div>
     );
   }
