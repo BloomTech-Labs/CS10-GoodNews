6ae773fb33500526fbbbb10810cab41bc1dc90bd
Res Yeg <shaqk702@gmail.com>
2018-10-04 21:24:51 -0700

    added tests for authenticated users

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -95,106 +95,48 @@ describe('server for non-authenticated users', () => {
 
 
 // tests server's User and Article routes for authenticated users
-// describe('server for authenticated users', () => {
-//     let token;
-//     beforeAll((done) => {
-//         request(server).post('/api/user/register').send({username: 'user',password: 'pw'}).end((err, response) => {
-//             console.log(response);
-//             token = response.body.token; // save the token!
-//             done();
-//         });
-//     });
-//     test('/api/user method-GET-getAll should require authorization', () => {
-//         return request(server).get('/api/user').then((response) => {
-//             expect(response.status).toBe(404);
-//         });
-//     });
-    // test('/api/user method-GET-getAll responds with JSON and 403', () => {
-    //     return request(server).get('/api/user').set('Authorization', `Bearer ${token}`)
-    //     .then((response) => {
-    //         expect(response.status).toBe(403);
-    //         expect(response.type).toBe('application/json');
-    //     });
-    // });
-    // test('/api/article/post-article method-POST-post ', () => {
-    //     const mockArticle = { name: 'NYT: White House eyes 12 potential authors behind anonymous op-ed', 
-    //                     url: 'https://www.cnn.com/2018/09/07/politics/trump-new-york-times-op-ed-suspects/index.html',
-    //                     clickbait: '0' };
-    //     const newArticle = Article.create(mockArticle); 
-    //     return request(server).post('/api/article/post-article').set('Authorization', `Bearer ${token}`).send(newArticle)
-    //     .then((response) => {
-    //         expect(response.status).toBe(403);
-    //         expect(response.type).toBe('application/json');
-    //     });
-    // });
+describe('server for authenticated users', () => {
+    let token;
+    beforeAll((done) => {
+        return mongoose
+            .connect(
+                'mongodb://127.0.0.1/goodnews-test',
+                { useNewUrlParser: true }
+            )
+            .then(() => console.log('connected to db'))
+            .then(() => {
+                request(server).post('/api/user/register').send({username: 'user',password: 'pw'}).end((err, response) => {
+                    // console.log(response);
+                    token = response.body.token; // save the token!
+                    done();
+                });
+            })
+    });
 
-    // User Tests
-    // test('/api/user method-GET-getAll should return 200 and a response as JSON object', async () => {
-    //     const mockUser = { email: 'user@email.com', password: '123' };
-    //     const newUser = await User.create(mockUser);
-    //     const response = await request(server).get('/api/user').set('Authorization', `Bearer ${token}`);
-    //     expect(response.status).toBe(200);
-    //     expect(response.type).toBe('application/json');
-    //     await mongoose.connection.db.dropCollection('users');
-    // });
-    // test('/api/user/login method-POST-postLoginUser should return 200', async () => {
-    //     const mockUser = { email: 'johnsmith@gmail.com', password: '123' };
-    //     const newUser = await User.create(mockUser);
-    //     const response = await request(server).get(`/api/user/${newUser._id}`);
-    //     expect(response.status).toBe(200);
-    //     await mongoose.connection.db.dropCollection('users');
-    // });
-    // test('/api/user/logged method-GET-getById should return 200', async () => {
-    //     const mockUser = { email: 'johnsmith@gmail.com', password: '123' };
-    //     const newUser = await User.create(mockUser);
-    //     const response = await request(server).get(`/api/user/${newUser._id}`);
-    //     expect(response.status).toBe(200);
-    //     await mongoose.connection.db.dropCollection('users');
-    // });
-    // test('User - test 4: PUT should return 201', async () => {
-    //     const mockUser = { email: 'johnsmith@gmail.com', password: '123' };
-    //     const newUser = await User.create(mockUser);
-    //     const response = await request(server).put(`/api/user/${newUser.id}`).send((newUser.email = 'johnsmith@gmail.com'));
-    //     expect(response.status).toBe(201);
-    //     await mongoose.connection.db.dropCollection('users');
-    // });
-    // test('User - test 5: DELETE should return 204', async () => {
-    //     const mockUser = { email: 'johnsmith@gmail.com', password: '123' };
-    //     const newUser = await User.create(mockUser);
-    //     const response = await request(server).delete(`/api/user/${newUser._id}`);
-    //     expect(response.status).toBe(204);
-    //     await mongoose.connection.db.dropCollection('users');
-    // });
-
-    // Article Tests
-    // test('Article - test 1: should return 200 and a response', async () => {
-    //     const mockArticle = { name: 'NYT: White House eyes 12 potential authors behind anonymous op-ed', 
-    //         url: 'https://www.cnn.com/2018/09/07/politics/trump-new-york-times-op-ed-suspects/index.html' };
-    //     const newArticle = await Article.create(mockArticle);
-    //     const response = await request(server).get('/api/article').send(newArticle);
-    //     expect(response.status).toBe(200);
-    //     expect(newArticle.url).toBe(mockArticle.url);
-    //     await mongoose.connection.db.dropCollection('articles');
-    // });
-    // test('Article - test 2: should return 200 and a response', async () => {
-    //     const mockArticle = { name: 'NYT: White House eyes 12 potential authors behind anonymous op-ed', 
-    //         url: 'https://www.cnn.com/2018/09/07/politics/trump-new-york-times-op-ed-suspects/index.html' };
-    //     const newArticle = await Article.create(mockArticle);
-    //     const response = await request(server).get(`/api/article/${newArticle._id}`);
-    //     expect(response.status).toBe(200);
-    //     expect(newArticle.name).toBe(mockArticle.name);
-    //     await mongoose.connection.db.dropCollection('articles');
-    // });
-    // test('Article - test 3: should return 200 and a response', async () => {
-    //     const mockArticle = {name: 'NYT: White House eyes 12 potential authors behind anonymous op-ed', 
-    //         url: 'https://www.cnn.com/2018/09/07/politics/trump-new-york-times-op-ed-suspects/index.html'};
-    //     const newArticle = await Article.create(mockArticle);
-    //     const mockUser = { email: 'johnsmith@gmail.com', password: '123' };
-    //     const newUser = await User.create(mockUser);
-    //     await newUser.saved_articles.push(newArticle._id);
-    //     const response = await request(server).get(`/api/article/${newArticle._id}`).send(newUser.saved_articles);
-    //     expect(response.status).toBe(200);
-    //     await mongoose.connection.db.dropCollection('articles');
-    //     await mongoose.connection.db.dropCollection('users');
-    // });
-// });
+    afterAll((done) => {
+        return mongoose.disconnect(done);
+    });
+    test('/api/user method-GET-getAll should require authorization', () => {
+        return request(server).get('/api/user').then((response) => {
+            expect(response.status).toBe(404);
+        });
+    });
+    test('/api/user method-GET-getAll responds with JSON and 403', () => {
+        return request(server).get('/api/user').set('Authorization', `Bearer ${token}`)
+        .then((response) => {
+            expect(response.status).toBe(403);
+            expect(response.type).toBe('application/json');
+        });
+    });
+    test('/api/article/post-article method-POST-post ', () => {
+        const mockArticle = { name: 'NYT: White House eyes 12 potential authors behind anonymous op-ed', 
+                        url: 'https://www.cnn.com/2018/09/07/politics/trump-new-york-times-op-ed-suspects/index.html',
+                        clickbait: '0' };
+        const newArticle = Article.create(mockArticle); 
+        return request(server).post('/api/article/post-article').set('Authorization', `Bearer ${token}`).send(newArticle)
+        .then((response) => {
+            expect(response.status).toBe(403);
+            expect(response.type).toBe('application/json');
+        });
+    });
+});
