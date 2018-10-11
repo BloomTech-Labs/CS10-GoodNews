2f2357db6e3abcc52c29781df01bd3b9598028fb
Res Yeg <shaqk702@gmail.com>
2018-10-11 10:29:34 -0700

    fixed bugs in ArticleRouterDS

--- Article/ArticleRouterDS.js
+++ Article/ArticleRouterDS.js
@@ -3,28 +3,30 @@ const Article = require('./Article')
 const urlDs = 'https://lab7goodnews-ds.herokuapp.com/stories'
 
 module.exports = function () {
-  setInterval(() => {
-    let newTimestamp = new Date(Date.now() - 1 * 60 * 60 * 1000)
-    newTimestamp = newTimestamp.toISOString()
-    https.get(`${urlDs}/?timestamp="${newTimestamp}"`, (res) => {
-      res.setEncoding('utf8')
-      let rawData = ''
-      res.on('data', (chunk) => {
-        rawData += chunk
+  return (
+    setInterval(() => {
+      let newTimestamp = new Date(Date.now() - 1 * 60 * 60 * 1000)
+      newTimestamp = newTimestamp.toISOString()
+      https.get(`${urlDs}/?timestamp="${newTimestamp}"`, (res) => {
+        res.setEncoding('utf8')
+        let rawData = ''
+        res.on('data', (chunk) => {
+          rawData += chunk
+        })
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData)
+            Article
+              .insertMany(parsedData, (err, data) => {
+                if (err) console.log('insertMany error: ', err)
+              })
+          } catch (e) {
+            console.error('catch error: ', e.message)
+          }
+        })
+      }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`)
       })
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData)
-          Article
-            .insertMany(parsedData, (err, data) => {
-              if (err) console.log('insertMany error: ', err)
-            })
-        } catch (e) {
-          console.error('catch error: ', e.message)
-        }
-      })
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`)
-    })
-  }, 1800000)
+    }, 1800000)
+  );
 }
